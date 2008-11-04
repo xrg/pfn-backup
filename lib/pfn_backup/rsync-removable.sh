42e74cd850e91efc93b9b110c30a846907326093
P. Christeas <p_christ@hol.gr>
2008-11-04 13:28:29 +0200

    Improve rsync-removable.

--- lib/pfn_backup/rsync-removable.sh
+++ lib/pfn_backup/rsync-removable.sh
@@ -5,22 +5,24 @@
 # by P. Christeas, (c) 2005-6
 # This is free software!
 
+set -e
 
-FS_LABEL='Backup'
+FS_LABEL=''
 DO_BEEP=yes
+BEEP_CMD=true
 BEEP_OPTS="-e /dev/tty0"
 MNTPOINT=/mnt/backup
 OUT='/bin/logger -t backup'
-FOUND_LABEL=""
-FOUND_DEVICE="/dev/disk/by-label/$FS_LABEL"
+FOUND_LABEL="$HAL_PROP_VOLUME_LABEL"
+FOUND_DEVICE="$HAL_PROP_BLOCK_DEVICE"
 DO_VERBOSE=
-BKUP_SRC=/koina/home/backup/
+BKUP_SRC=/var/backup/
 BKUP_DEST=/upload/$(hostname)
 RSYNC_OPTS=
 # RSYNC_OPTS="--delete"
 
-[ -f /etc/backup/sync-removable.conf ] && \
-	. /etc/backup/sync-removable.conf
+[ -f /etc/backup/options ] && \
+	. /etc/backup/options
 
 
 while getopts "qfd:l:v" OPTION ; do
@@ -32,7 +34,7 @@ while getopts "qfd:l:v" OPTION ; do
 		OUT=cat
 	;;
 	d)
-		FOUND_DEVICE="/dev/$OPTARG"
+		FOUND_DEVICE="$OPTARG"
 	;;
 	l)
 		FOUND_LABEL="$OPTARG"
@@ -43,8 +45,20 @@ while getopts "qfd:l:v" OPTION ; do
 	esac
 done
 
+if [ ! -b "$FOUND_DEVICE" ] ; then
+	echo "Device \"$FOUND_DEVICE\" is not a block one!"
+	exit 3
+fi
 
-if [ "$FOUND_LABEL" != "$FS_LABEL" ] ; then
+if [ -n "$BACKUP_DIR" ] ; then
+	BKUP_SRC="$BACKUP_DIR/gpg"
+fi
+
+if [ "$DO_BEEP" == "yes" ] ; then
+	 BEEP_CMD="$(which beep) $BEEP_OPTS"
+fi
+
+if [ -n "$FS_LABEL" ] && [ "$FOUND_LABEL" != "$FS_LABEL" ] ; then
 	[ "$DO_VERBOSE" == 'y' ] && (echo "Label mismatch" | $OUT )
 	exit
 fi
@@ -53,37 +67,24 @@ if [ ! -d "$MNTPOINT" ] ; then
 	mkdir "$MNTPOINT" | $OUT
 fi
 
-if [ ! -b "$FOUND_DEVICE" ] ; then
-	[ "$DO_VERBOSE" == 'y' ] && (echo "Bkup dev not there, waiting 1 sec" | $OUT )
-	sleep 10
+mount "$FOUND_DEVICE" $MNTPOINT | $OUT
 
-	if [ ! -b "$FOUND_DEVICE" ] ; then
-		echo "Backup dev '$FOUND_DEVICE' not present" | $OUT
-# 		exit 0
-	fi
-fi
+$BEEP_CMD -f 1000 -n -f 10 -l 50 -n -f 2000 -n -d 10 -l 100
 
-if ! mount -t ext3 "$FOUND_DEVICE" $MNTPOINT | $OUT ; then
-	exit 1
-fi
-
-[ ! -z "$DO_BEEP" ] && beep -f 1000 -n -f 10 -l 50 -n -f 2000 -n -d 10 -l 100 $BEEP_OPTS
-
-BEEP_CMD='cat'
-if [ ! -z "$DO_BEEP" ] ; then
-	BEEP_CMD="beep -s -f 400 -D 20 -l 10 $BEEP_OPTS"
-fi
+BEEP_CMD2="$BEEP_CMD -s -f 400 -D 20 -l 10"
 
 if [ -f $MNTPOINT/signature ] && cmp /root/signature $MNTPOINT/signature ; then
 	echo "Performing backup!" | $OUT
-	nice -n 10 rsync -a $RSYNC_OPTS -P -q "$BKUP_SRC" "$MNTPOINT/$BKUP_DEST" | $BEEP_CMD | $OUT
+	[ "$DO_VERBOSE" == 'y' ] && (echo "rsync a $RSYNC_OPTS -P -q $BKUP_SRC $MNTPOINT/$BKUP_DEST" | $OUT )
+	nice -n 10 rsync -a $RSYNC_OPTS -P -q "$BKUP_SRC" "$MNTPOINT/$BKUP_DEST" | $BEEP_CMD2 2| $OUT
 	umount $MNTPOINT | $OUT
 	echo "Sync done,USB can be removed." | $OUT
-	[ ! -z "$DO_BEEP" ] && beep -f 2000 -n -f 10 -l 50 -n -f 1000 $BEEP_OPTS
+	$BEEP_CMD -f 2000 -n -f 10 -l 50 -n -f 1000
 else
+	sleep 1
 	echo "Signature not found! Disk may be spoofed!" | $OUT
-	umount $MNTPOINT | $OUT
-	[ ! -z "$DO_BEEP" ] && beep -f 200 -l 500 $BEEP_OPTS
+	umount $MNTPOINT | $OUT || true
+	$BEEP_CMD -f 200 -l 500
 	exit 2
 fi
 
